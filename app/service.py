d8ac97525c1d654fb29b448359fec417223dde86
Blas Isaias Fernández <blasferna@gmail.com>
2022-03-21 17:45:18 +0000

    Get data from SET ws

--- app/service.py
+++ app/service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
-from . import ips, models
+from . import _set, ips, models
 from .db import db, db_1
 from .utils import PrettyJSONResponse, int_to_datestr
 
@@ -13,11 +13,14 @@ async def get_ruc(ruc):
     if data is None:
         data = await models.Persona.get_ruc(db_1, ruc)
         if data is None:
-            raise HTTPException(status_code=404, detail="No encontrado")
+            try:
+                data = _set.get_taxpayer(ruc)
+            except (Exception, _set.DoesNotExist) as e:
+                raise HTTPException(status_code=404, detail="No encontrado")
     elif data.tipo == "F":
         _data = await models.Persona.get_ruc(db_1, ruc)
         if _data is not None:
-            _data['dv'] = data.dv
+            _data["dv"] = data.dv
             data = _data
     return data
 
@@ -28,7 +31,8 @@ async def get_ips(documento):
         return ips.consulta_asegurado(documento)
     except ips.IPSException:
         raise HTTPException(
-            status_code=502, detail="No se pudo obtener una respuesta a tiempo")
+            status_code=502, detail="No se pudo obtener una respuesta a tiempo"
+        )
 
 
 @app.get("/personas", response_class=PrettyJSONResponse)
@@ -36,17 +40,20 @@ async def get_persona(cedula):
     result = await models.Persona.retreive(db_1, cedula)
     if result is None:
         ruc = await models.Ruc.retreive(db, cedula)
-        if ruc is None or ruc.tipo != 'F':
-            raise HTTPException(status_code=404, detail="Persona no encontrada")
+        if ruc is None or ruc.tipo != "F":
+            try:
+                return _set.get_citizen(cedula)
+            except (Exception, _set.DoesNotExist) as e:
+                raise HTTPException(status_code=404, detail="Persona no encontrada")
         return {
             "cedula": ruc.ruc,
             "apellidos": "",
             "nombres": ruc.razonsocial,
-            "fecNac": None
+            "fecNac": None,
         }
     return {
         "cedula": result.id,
         "apellidos": result.apellidos,
         "nombres": result.nombres,
-        "fecNac": int_to_datestr(result.fecnac)
+        "fecNac": int_to_datestr(result.fecnac),
     }
--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,10 @@
+import base64
 import json
 import typing
 from datetime import datetime, timedelta
 
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
 from starlette.responses import Response
 
 
@@ -22,3 +25,13 @@ def int_to_datestr(value):
     s = str(value)
     fecnac = datetime(year=int(s[0:4]), month=int(s[4:6]), day=int(s[6:8]))
     return fecnac.strftime("%d/%m/%Y")
+
+
+def encrypt_param(param, variant="ruc"):
+    encryption_key = base64.b64decode("aCIbjMuVGtwF8nlSKoPydE==")
+    text = json.dumps({variant: param}).encode()
+    text_padded = Padding.pad(text, AES.block_size)
+    iv = base64.b64decode("JAwlt7SNbYLycmPRqeDFou==")
+    cipher = AES.new(encryption_key, AES.MODE_CBC, iv)
+    cipher_enc = cipher.encrypt(text_padded)
+    return base64.b64encode(cipher_enc).decode()
