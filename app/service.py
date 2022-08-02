e8712b34204659b323b222ef1563361af2229874
blasferna <blasferna@gmail.com>
2022-08-02 15:45:30 -0400

    New endpoint validate-ruc

--- app/service.py
+++ app/service.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI, HTTPException
+import csv
+from io import BytesIO, StringIO
+from typing import Optional
 
-from . import _set, ips, models
+import pandas as pd
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from fastapi.responses import HTMLResponse
+
+from . import _set, ips, mimetypes, models
 from .db import db, db_1
-from .utils import PrettyJSONResponse, int_to_datestr
+from .utils import PrettyJSONResponse, get_delimitter, int_to_datestr
 
 app = FastAPI()
 
@@ -58,3 +64,65 @@ async def get_persona(cedula):
         "nombres": result.nombres,
         "fecNac": int_to_datestr(result.fecnac),
     }
+
+
+@app.post("/validate-ruc", response_class=PrettyJSONResponse)
+async def create_upload_file(
+    file: Optional[UploadFile] = File(None), index: str = Form("0")
+):
+    if file.content_type not in [
+        mimetypes.TEXT_PLAIN,
+        mimetypes.CSV,
+        mimetypes.MS_EXCEL,
+        mimetypes.SHEET,
+    ]:
+        raise HTTPException(
+            status_code=400,
+            detail=f"File type of {file.content_type} is not supported",
+        )
+
+    df = None
+    offset = 0
+
+    if file.content_type == mimetypes.SHEET:
+        offset = 1
+        contents = await file.read()
+        data = BytesIO(contents)
+        df = pd.read_excel(data, header=None)
+
+    if file.content_type in (mimetypes.TEXT_PLAIN, mimetypes.CSV):
+        offset = 2
+        buffer = StringIO(str(file.file.read(), "utf-8"))
+        delimiter = get_delimitter(buffer)
+        df = pd.read_csv(buffer, header=None, encoding="utf-8", sep=delimiter)
+
+    if df is None:
+        raise HTTPException(status_code=400, detail="Missing dataframe")
+
+    rucs = [str(row[int(index)]) for i, row in df.iterrows()]
+    data = await models.Ruc.get_dict(db, rucs)
+
+    invalid_list = []
+    for i, row in df.iterrows():
+        raw = " ".join([str(x) for x in row])
+        ruc = str(row[int(index)])
+        instance = data.get(ruc)
+        if instance is None:
+            invalid_list.append(
+                {
+                    "row": i + offset,
+                    "data": raw,
+                    "ruc": ruc,
+                    "msg": "RUC INEXISTENTE",
+                }
+            )
+        else:
+            if instance.estado != "ACTIVO":
+                invalid_list.append(
+                    {
+                        "row": i + offset,
+                        "data": raw,
+                        "msg": instance.estado,
+                    }
+                )
+    return {"invalid_list": invalid_list}
