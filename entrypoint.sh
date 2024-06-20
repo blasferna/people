7532a35edee3cd42afea6012f0cb125c0c0c6478
blasferna <blasferna@gmail.com>
2024-06-20 06:45:19 -0400

    refactor: Update entrypoint script to handle additional command options
    - Modify the entrypoint script to handle the "build" and "download" commands.
    - If the command is either "build" or "download", execute the corresponding app command.
    - Otherwise, execute the provided command as before.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 if [ "$#" -eq 0 ]; then
     exec python manage.py runserver --host=0.0.0.0 --port=80
 else
-    exec "$@"
+    if [ $# -eq 1 ] && ([[ $1 == "build" ]] || [[ $1 == "download" ]]); then
+        exec python manage.py "$1"
+    else
+        exec "$@"
+    fi
 fi
